b53afd5ac09f6789dd47fcc0b2b52d8818e14ac4
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2024-02-01 00:06:10 -0600

    Agregar btn procesar

--- app/src/main/java/com/example/pm012024p1/MainActivity.java
+++ app/src/main/java/com/example/pm012024p1/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.pm012024p1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nombres, apellidos, telefono;
+    Button BtnProcesar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nombres = (EditText) findViewById(R.id.nombres);
+        apellidos = (EditText) findViewById(R.id.apellidos);
+        telefono = (EditText) findViewById(R.id.telefono);
+        BtnProcesar = (Button) findViewById(R.id.BtnProcesar);
+
+        BtnProcesar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), nombres.getText().toString(), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
